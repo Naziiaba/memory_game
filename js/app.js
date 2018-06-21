7b0b0094e3c1094982fde4b6bc40403413f060bb
Zia <35944002+Ziarellie@users.noreply.github.com>
2018-06-21 14:56:46 -0400

    A list of cards
    Added a list of holds all of the cards.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@
  */
 
 
+const listOfCards = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-bolt",
+        "fa fa-cube", "fa fa-anchor", "fa fa-leaf", "fa fa-bicycle", "fa fa-diamond", "fa fa-bomb",
+        "fa fa-leaf", "fa fa-bomb", "fa fa-bolt", "fa fa-bicycle", "fa fa-paper-plane-o", "fa fa-cube"];
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
